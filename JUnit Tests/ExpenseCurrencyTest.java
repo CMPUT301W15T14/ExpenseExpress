17d82c5337d5ec02824b70abee0c7cb31893e6e9
hzou <hzou@ualberta.ca>
2015-02-11 20:49:04 -0700

    Update ExpenseCurrencyTest.java

--- JUnit Tests/ExpenseCurrencyTest.java
+++ JUnit Tests/ExpenseCurrencyTest.java
@@ -1,1 +1,47 @@
-# Tests Use Case 4.3
+import java.util.ArrayList;
+import java.util.GregorianCalendar;
+import team14.expenseexpress.ClaimModel;
+import team14.expenseexpress.Controller;
+import team14.expenseexpress.ExpenseModel;
+import android.test.ActivityInstrumentationTestCase2;
+
+
+public class ExpenseCurrencyTest extends
+		ActivityInstrumentationTestCase2<ExpenseActivity> {
+
+    private Controller controller;
+    private ExpenseModel expense;
+    private ClaimModel claim;
+    private final int currency = 1;
+	
+	public ExpenseCurrencyTest(){
+		super(AddExpenseActivity.class);
+	}
+	
+	protected void setUp() throws Exception {
+		super.setUp();
+		// Mock controller initialized with a reference to the claim
+		claim = new ClaimModel();
+	    controller = new Controller(claim);
+		// Preconditions: an expense item is initialized aleady
+		expense = controller.newExpense();
+	    ArrayList<ExpenseModel> expenses = claim.getExpenses();
+	    expenses.add(expense);
+	}
+	        
+	
+	public void testSetCategory(){
+	    int index = expenses.indexOf(expense);
+	    /* Mock controller :
+	        public void setExpenseCurrency(index, currency){
+	            ExpenseModel expense = expenses.get(index);
+	            expense.setCurrency(currency);
+	        }
+	    */
+	    controller.setExpenseCurrency(index, currency);
+	    assertEquals("The currency has been successfully set", controller.getExpenseCurrency(index), currency);
+	    // Pretend: static ArrayList<Integer> sCurrencies holds predefined currencies, in the ExpenseModel class
+	    // An alternative implementation could be a hash or an ArrayList<String> where the currency int is its index
+	    assertTrue("The currency is one from a preset list", ExpenseModel.sCurrencies.contains(Integer(currency)));
+	}
+}
