fb1b30ae148178a8d9035c32fbc855c93732f45b
pkuczera <pkuczera@ualberta.ca>
2015-02-13 10:42:46 -0700

    ApproveReturnClaimTest.java added.   -pkuczera

---
+++ JUnit Tests/ApproveReturnClaimTest.java
@@ -0,0 +1,12 @@
+import android.test.ActivityInstrumentationTestCase2;
+
+
+public class ApproveReturnClaimTest extends ActivityInstrumentationTestCase2<ClaimListActivity>  {
+	public ApproveReturnClaimTest() {
+        super(ClaimListActivity.class);
+    }
+    
+    protected void setUp() throws Exception {
+    	super.setUp();	
+    }
+}
