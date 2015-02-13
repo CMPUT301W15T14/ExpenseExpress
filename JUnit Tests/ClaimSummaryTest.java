41339cf0ee0033f6b7b4025773af245342ddfb08
pkuczera <pkuczera@ualberta.ca>
2015-02-13 08:20:39 -0700

    ClaimSummaryTest added.  -pkuczera

---
+++ JUnit Tests/ClaimSummaryTest.java
@@ -0,0 +1,12 @@
+import android.test.ActivityInstrumentationTestCase2;
+
+
+public class ClaimSummaryTest extends ActivityInstrumentationTestCase2<ClaimSummaryActivity> {
+	public ClaimSummaryTest() {
+		super(ClaimSummaryActivity);
+	}
+	
+	protected void setUp() throws Exception {
+		
+	}
+}
