707e2e279bb6e281da1e8182e84b64d9648c1e80
hzou <hzou@ualberta.ca>
2015-03-14 15:45:36 -0600

    done

--- src/team14/expenseexpress/controller/TagListController.java
+++ src/team14/expenseexpress/controller/TagListController.java
@@ -1,16 +1,40 @@
 package team14.expenseexpress.controller;
 
+import team14.expenseexpress.model.ClaimTag;
+import team14.expenseexpress.model.TagList;
 import android.content.Context;
 
 /**
- * Created by John on 2/19/2015.
+ * Singleton controller for tags.
+ * 
  */
 public class TagListController {
 	
 	private TagList tagList;
 	
-	public TagListController(TagList tagList){
-		this.tagList = tagList;
+	// singleton
+	private static TagListController instance;
+	
+	private TagListController(Context context){
+		tagList = TagList.getInstance(context);
 	}
 	
+	public static TagListController getInstance(Context context){
+		if (instance == null){
+			instance = new TagListController(context);
+		}
+		return instance;
+	}
+	
+	public void addTag(ClaimTag tag){
+		tagList.add(tag);
+	}
+	
+	public void removeTag(ClaimTag tag){
+		tagList.remove(tag);
+	}
+	
+	public void setTagName(ClaimTag tag, String name){
+		tag.setName(name);
+	}
 }
