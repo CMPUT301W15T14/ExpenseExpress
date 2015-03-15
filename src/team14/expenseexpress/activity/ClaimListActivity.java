13c452961dfb011dcca77e70541e118faf6c961e
hzou <hzou@ualberta.ca>
2015-03-14 22:47:01 -0600

    Extracted TagListDialogFragment from ClaimListActivity

--- src/team14/expenseexpress/activity/ClaimListActivity.java
+++ src/team14/expenseexpress/activity/ClaimListActivity.java
@@ -5,12 +5,12 @@ import java.util.ArrayList;
 import team14.expenseexpress.CustomBaseAdapter;
 import team14.expenseexpress.ExpenseExpressActivity;
 import team14.expenseexpress.R;
+import team14.expenseexpress.activity.TagListDialogFragment.TagsListAdapter;
 import team14.expenseexpress.controller.ClaimController;
 import team14.expenseexpress.model.Claim;
 import team14.expenseexpress.model.ClaimList;
 import team14.expenseexpress.model.ClaimTag;
 import team14.expenseexpress.model.TagList;
-import team14.expenseexpress.util.LocalFileHelper;
 import android.annotation.SuppressLint;
 import android.app.Dialog;
 import android.app.DialogFragment;
@@ -28,240 +28,169 @@ import android.view.ViewGroup;
 import android.view.Window;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
-import android.widget.BaseAdapter;
-import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 
 public class ClaimListActivity extends ExpenseExpressActivity {
 
-    private ArrayList<ClaimTag> claimTags;
-    private ArrayList<ClaimTag> chosenTags;
-    private ArrayList<Claim> claimList;
-    private ClaimController cListController;
-    private TagListDialogFragment.TagsListAdapter tagsListAdapter;
-    private CustomBaseAdapter adapter;
- 
-    
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_claim_list);
-        claimTags = TagList.getInstance(this).get();
-        chosenTags = new ArrayList<ClaimTag>();
-        LayoutInflater.from(this);
-        
-        // TODO
-        cListController = ClaimController.getInstance();
-        cListController.initialize(this);
-        claimList = ClaimList.getInstance().getClaims();
-        
-        final ListView lv1 = (ListView) findViewById(R.id.claimListView);
+	private ArrayList<ClaimTag> claimTags;
+	private ArrayList<ClaimTag> chosenTags;
+	private ArrayList<Claim> claimList;
+	private ClaimController cListController;
+	private CustomBaseAdapter adapter;
+	private TagsListAdapter tagsListAdapter;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_claim_list);
+		claimTags = TagList.getInstance(this).get();
+		chosenTags = new ArrayList<ClaimTag>();
+		LayoutInflater.from(this);
+
+		// TODO
+		cListController = ClaimController.getInstance();
+		cListController.initialize(this);
+		claimList = ClaimList.getInstance().getClaims();
+
+		final ListView lv1 = (ListView) findViewById(R.id.claimListView);
 		CustomBaseAdapter adapter = new CustomBaseAdapter(this, claimList);
 		lv1.setAdapter(adapter);
 		registerForContextMenu(lv1);
 		lv1.setOnItemClickListener(new OnItemClickListener() {
 			@Override
-			public void onItemClick(AdapterView<?> parent, View view,int position, long id) {
-				
+			public void onItemClick(AdapterView<?> parent, View view,
+					int position, long id) {
+
 				Claim claim = (Claim) lv1.getItemAtPosition(position);
 				cListController.setSelectedClaim(claim);
-				Intent intent = new Intent(ClaimListActivity.this, ExpenseListActivity.class);
+				Intent intent = new Intent(ClaimListActivity.this,
+						ExpenseListActivity.class);
 				startActivity(intent);
 			}
-        });
-    }
-    
-    @SuppressLint("ValidFragment")
-	private class TagListDialogFragment extends DialogFragment{
-    	
-    	
-    	@Override
-    	public Dialog onCreateDialog(Bundle savedInstanceState) {
-    	  Dialog dialog = super.onCreateDialog(savedInstanceState);
+		});
+	}
 
-    	  // request a window without the title
-    	  dialog.getWindow().requestFeature(Window.FEATURE_NO_TITLE);
-    	  return dialog;
-    	}
-    	
+	@SuppressLint("ValidFragment")
+	private class NewTagDialogFragment extends DialogFragment {
+
+		@Override
+		public Dialog onCreateDialog(Bundle savedInstanceState) {
+			Dialog dialog = super.onCreateDialog(savedInstanceState);
+
+			// request a window without the title
+			dialog.getWindow().requestFeature(Window.FEATURE_NO_TITLE);
+			return dialog;
+		}
+
 		@Override
 		public View onCreateView(LayoutInflater inflater, ViewGroup container,
 				Bundle savedInstanceState) {
-			
-			View v = inflater.inflate(R.layout.activity_tags, container, false);
-			tagsListAdapter = new TagsListAdapter();
-			(v.findViewById(R.id.addNewTagButton)).setOnClickListener(new OnClickListener(){
-				@Override
-				public void onClick(View view) {
-					showNewTagDialog();
-				}
-			});
-			((ListView) v.findViewById(R.id.ExpenseList)).setAdapter(tagsListAdapter);
-			return v;
-		}
+			final View view = inflater.inflate(R.layout.activity_add_tag,
+					container, false);
+			view.findViewById(R.id.newTagButton).setOnClickListener(
+					new OnClickListener() {
 
-
-		private class TagsListAdapter extends BaseAdapter{
-
-			@Override
-			public int getCount() {
-				// TODO Auto-generated method stub
-				return claimTags.size();
-			}
-
-			@Override
-			public Object getItem(int arg0) {
-				/* not needed */
-				return null;
-			}
-
-			@Override
-			public long getItemId(int arg0) {
-				/* not needed */
-				return 0;
-			}
-
-			@Override
-			public View getView(final int position, final View convertView, ViewGroup parent) {
-				// Recycles convertView
-				// TODO: put controller in between View and Model
-				final ClaimTag tag = claimTags.get(position);
-				
-				CheckBox checkBox = (CheckBox) convertView;
-				if (checkBox==null){
-					checkBox = new CheckBox(ClaimListActivity.this);
-				}
-				
-				if (chosenTags.contains(tag)){
-					checkBox.setChecked(true);
-				}
-				
-				checkBox.setText(tag.getName());
-				
-				checkBox.setOnClickListener(new OnClickListener() {
-					
-					@Override
-					public void onClick(View v) {
-						// Is the view now checked?
-					    boolean checked = ((CheckBox) v).isChecked();
-			            if (checked){
-			            	chosenTags.add(tag);
-			            } else {
-			            	chosenTags.remove(tag);
-			            }
-					}
-				});
-				
-				return checkBox;
-			}
-		}
-    	
-    }
-    
-    
-    @SuppressLint("ValidFragment")
-	private class NewTagDialogFragment extends DialogFragment{
-    	
-    	@Override
-    	public Dialog onCreateDialog(Bundle savedInstanceState) {
-    	  Dialog dialog = super.onCreateDialog(savedInstanceState);
-
-    	  // request a window without the title
-    	  dialog.getWindow().requestFeature(Window.FEATURE_NO_TITLE);
-    	  return dialog;
-    	}
-    	
-		@Override
-		public View onCreateView(LayoutInflater inflater, ViewGroup container,
-				Bundle savedInstanceState) {
-			final View view = inflater.inflate(R.layout.activity_add_tag, container, false);
-			view.findViewById(R.id.newTagButton).setOnClickListener(new OnClickListener(){
-
-				@Override
-				public void onClick(View v) {
-					String text = ((EditText) view.findViewById(R.id.newTagNameField)).getText().toString();
-					if (text.length()==0){
-						toast("Tag name can't be empty");
-					} else {
-						ClaimTag tag = new ClaimTag(text);
-						// check if it's already in list
-						if (claimTags.contains(tag)){
-								toast("Already in list");
-								return;
+						@Override
+						public void onClick(View v) {
+							String text = ((EditText) view
+									.findViewById(R.id.newTagNameField))
+									.getText().toString();
+							if (text.length() == 0) {
+								toast("Tag name can't be empty");
+							} else {
+								ClaimTag tag = new ClaimTag(text);
+								// check if it's already in list
+								if (claimTags.contains(tag)) {
+									toast("Already in list");
+									return;
+								}
+								// if all good, add to list and dismiss dialog
+								claimTags.add(tag);
+								toast("Added to list");
+								updateTagsListAdapter();
+								dismiss();
+							}
 						}
-						// if all good, add to list and dismiss dialog
-						claimTags.add(tag);
-						toast("Added to list");
-						updateTagsListAdapter();
-						dismiss();
-					}
-				}
-			});
+					});
 			return view;
 		}
-    }
-    
-    @Override
+	}
+
+	@Override
 	public void onCreateContextMenu(ContextMenu menu, View v,
 			ContextMenuInfo menuInfo) {
-		String[] menuItems = getResources().getStringArray(R.array.LongClickMenu);
-		for (int i = 0; i<menuItems.length; i++) {
+		String[] menuItems = getResources().getStringArray(
+				R.array.LongClickMenu);
+		for (int i = 0; i < menuItems.length; i++) {
 			menu.add(Menu.NONE, i, i, menuItems[i]);
 		}
 	}
-    
-    @Override
-	public boolean onContextItemSelected(MenuItem item) {
-		 
-		 final ListView lv1 = (ListView) findViewById(R.id.claimListView);
-		 AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
-		 int menuItemIndex = item.getItemId();
-		 String[] menuItems = getResources().getStringArray(R.array.LongClickMenu);
-		 String menuItemName = menuItems[menuItemIndex];
-		 
-		 Claim claim = (Claim) lv1.getItemAtPosition(info.position);
-		 
-		 if (menuItemName.equals("Delete")) {
-			 claimList.remove(claim);
-			 log(adapter.toString());
-			 adapter.notifyDataSetChanged();
-		 } else if (menuItemName.equals("Edit")) {
-			 if (claim.getStatus().equals("submitted") || (claim.getStatus().equals("approved"))) {
-				 Toast.makeText(this, "Cannot Edit Claim", Toast.LENGTH_SHORT).show();
-			 } else {
-				// startActivity(new Intent(ClaimListActivity.this, ClaimDetailsAddEditActivity.class);
-			 }
-		 } else if (menuItemName.equals("Details")) {
-			 startActivity(new Intent(ClaimListActivity.this, ClaimDetailsActivity.class));
-		 }
-		 return true;
-    }
-    
-    
-    private void updateTagsListAdapter(){
-    	tagsListAdapter.notifyDataSetChanged();
-    }
-        
-	private void showNewTagDialog(){
-		FragmentManager fm = getFragmentManager();
-        new NewTagDialogFragment().show(fm, "newTagDialogFragment");
-	}
-    
-    public void onClick_seeTags(View v) {
-    	FragmentManager fm = getFragmentManager();
-        new TagListDialogFragment().show(fm, "tagsListDialogFragment");
 
-    }
-    
-    public void onClick_newClaim(View v) {
-    	// TODO
-    	startActivity(new Intent(ClaimListActivity.this, NewClaimActivity.class));
-    }
-    
-    public void onClick_searchByTag(View v) {
-    	// TODO
-    }
+	@Override
+	public boolean onContextItemSelected(MenuItem item) {
+
+		final ListView lv1 = (ListView) findViewById(R.id.claimListView);
+		AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) item
+				.getMenuInfo();
+		int menuItemIndex = item.getItemId();
+		String[] menuItems = getResources().getStringArray(
+				R.array.LongClickMenu);
+		String menuItemName = menuItems[menuItemIndex];
+
+		Claim claim = (Claim) lv1.getItemAtPosition(info.position);
+
+		if (menuItemName.equals("Delete")) {
+			claimList.remove(claim);
+			log(adapter.toString());
+			adapter.notifyDataSetChanged();
+		} else if (menuItemName.equals("Edit")) {
+			if (claim.getStatus().equals("submitted")
+					|| (claim.getStatus().equals("approved"))) {
+				Toast.makeText(this, "Cannot Edit Claim", Toast.LENGTH_SHORT)
+						.show();
+			} else {
+				// startActivity(new Intent(ClaimListActivity.this,
+				// ClaimDetailsAddEditActivity.class);
+			}
+		} else if (menuItemName.equals("Details")) {
+			startActivity(new Intent(ClaimListActivity.this,
+					ClaimDetailsActivity.class));
+		}
+		return true;
+	}
+
+	public void setTagsListAdapter(TagsListAdapter adapter) {
+		this.tagsListAdapter = adapter;
+	}
+
+	private void updateTagsListAdapter() {
+		tagsListAdapter.notifyDataSetChanged();
+	}
+
+	public void showNewTagDialog() {
+		FragmentManager fm = getFragmentManager();
+		new NewTagDialogFragment().show(fm, "newTagDialogFragment");
+	}
+
+	public void onClick_seeTags(View v) {
+		FragmentManager fm = getFragmentManager();
+		new TagListDialogFragment(this).show(fm, "tagsListDialogFragment");
+
+	}
+
+	public void onClick_newClaim(View v) {
+		// TODO
+		startActivity(new Intent(ClaimListActivity.this, NewClaimActivity.class));
+	}
+
+	public void onClick_searchByTag(View v) {
+		// TODO
+	}
+
+	public ArrayList<ClaimTag> getChosenTags() {
+		return chosenTags;
+	}
 
 }
