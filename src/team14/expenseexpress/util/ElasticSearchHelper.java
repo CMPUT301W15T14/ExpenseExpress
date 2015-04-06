7f021622d5788a95546c651f368f57045c997814
pkuczera <pkuczera@ualberta.ca>
2015-04-05 22:28:09 -0600

    brandon derps

--- src/team14/expenseexpress/util/ElasticSearchHelper.java
+++ src/team14/expenseexpress/util/ElasticSearchHelper.java
@@ -1,21 +1,23 @@
 package team14.expenseexpress.util;
 
 
-import android.util.Log;
-
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.lang.reflect.Type;
 
+import android.util.Log;
 
 import com.google.gson.Gson;
 import com.google.gson.JsonIOException;
 import com.google.gson.JsonSyntaxException;
+import com.google.gson.reflect.TypeToken;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpDelete;
-import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.methods.HttpPut;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
@@ -23,7 +25,6 @@ import org.apache.http.impl.client.DefaultHttpClient;
 
 import team14.expenseexpress.controller.Mode;
 import team14.expenseexpress.model.Claim;
-import team14.expenseexpress.model.ClaimList;
 
 public class ElasticSearchHelper {
 	private static final String SUBMITTED_URL = "http://cmput301.softwareprocess.es:8080/cmput301w15t14/submitted";
@@ -92,5 +93,4 @@ public class ElasticSearchHelper {
 			e.printStackTrace();
 		}
 	}
-
 }
