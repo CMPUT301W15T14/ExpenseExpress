f756b8347abe8a94d919fa506600cdb4272a4f52
budinski11 <zbudinsk@ualberta.ca>
2015-04-06 19:10:19 -0600

    add by gps

--- src/team14/expenseexpress/activity/EditDestinations.java
+++ src/team14/expenseexpress/activity/EditDestinations.java
@@ -1,14 +1,22 @@
 package team14.expenseexpress.activity;
 
+import team14.expenseexpress.LocationActivity;
 import team14.expenseexpress.R;
 import team14.expenseexpress.controller.ClaimController;
+import team14.expenseexpress.controller.DestinationController;
 import team14.expenseexpress.model.Destination;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.app.Dialog;
 import android.app.DialogFragment;
 import android.app.FragmentManager;
+import android.content.Context;
+import android.content.Intent;
 import android.view.ContextMenu;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
@@ -19,14 +27,17 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 import android.view.ContextMenu.ContextMenuInfo;
 import android.view.View.OnClickListener;
 
-public class EditDestinations extends Activity {
-	
+public class EditDestinations extends Activity{
+	private NewDestinationDialogFragment fragment;
 
 	private ListView destinationListView;
 	private ArrayAdapter<Destination> destinationAdapter;
+
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -42,6 +53,8 @@ public class EditDestinations extends Activity {
 	@SuppressLint("ValidFragment")
 	private class NewDestinationDialogFragment extends DialogFragment {
 
+		View view;
+		
 		@Override
 		public Dialog onCreateDialog(Bundle savedInstanceState) {
 			Dialog dialog = super.onCreateDialog(savedInstanceState);
@@ -51,13 +64,40 @@ public class EditDestinations extends Activity {
 			return dialog;
 		}
 
+		public void update(){
+			final TextView latText = (TextView) view.findViewById(R.id.AddDestLatitude);
+			final TextView lngText = (TextView) view.findViewById(R.id.addDestLongitude);
+	    	latText.setText("Latitude: ");
+	    	lngText.setText("Longitude: ");
+	    	latText.setText(latText.getText() + String.valueOf(DestinationController.getInstance().getDestLatitude()));
+	    	lngText.setText(lngText.getText() + String.valueOf(DestinationController.getInstance().getDestLongitude()));
+		}
+		
 		@Override
 		public View onCreateView(LayoutInflater inflater, ViewGroup container,
 				Bundle savedInstanceState) {
 			
 			final View v = inflater.inflate(R.layout.activity_add_destinations, container, false);
+			view = v;
 			final EditText nameText = (EditText) v.findViewById(R.id.addDestinationTextField);
 			final EditText reasonText = (EditText) v.findViewById(R.id.addReasonTextField);
+			final TextView latText = (TextView) v.findViewById(R.id.AddDestLatitude);
+			final TextView lngText = (TextView) v.findViewById(R.id.addDestLongitude);
+			DestinationController.getInstance().makeSelectedDestination();
+			
+			v.findViewById(R.id.addDestGPSButton).setOnClickListener(
+					
+					new OnClickListener() {
+						
+						@Override
+						public void onClick(View v) {
+							 Intent intent = new Intent(getActivity(), LocationActivity.class);
+				             startActivity(intent);
+
+					}});
+	
+			
+			
 			v.findViewById(R.id.addDestinationButton2).setOnClickListener(
 					new OnClickListener() {
 
@@ -68,11 +108,11 @@ public class EditDestinations extends Activity {
 							if (name.length() == 0 || reason.length() == 0) {
 								return;
 							} else {
-								Destination destination = new Destination(name);
-								destination.setReason(reason);
-								destination.setDestination(name);
+								
+								DestinationController.getInstance().setDestReason(reason);
+								DestinationController.getInstance().setDestName(name);
 								// if all good, add to list and dismiss dialog
-								ClaimController.getInstance().getSelectedClaim().addDestination(destination);
+								ClaimController.getInstance().getSelectedClaim().addDestination(DestinationController.getInstance().getSelectedDestination());
 								destinationAdapter.notifyDataSetChanged();
 								dismiss();
 							}
@@ -110,7 +150,8 @@ public class EditDestinations extends Activity {
 	
 	public void addNewDestinations(View v) {
 		FragmentManager fm = getFragmentManager();
-		new NewDestinationDialogFragment().show(fm, "DestinationsDialogFragment");
+		fragment = new NewDestinationDialogFragment();
+		fragment.show(fm, "DestinationsDialogFragment");
 	}
 	
 	@Override
@@ -118,8 +159,24 @@ public class EditDestinations extends Activity {
 		finish();
 	}
 	
+	
+	
+	@Override
+	protected void onResume() {
+		super.onResume();
+		
+		if (fragment == null){
+			return;
+		} else {
+			fragment.update();
+		}
+	}
+
 	public void acceptDestinations(View v) {
 		finish();
 	}
+
+
+	}
 	
-}
+
